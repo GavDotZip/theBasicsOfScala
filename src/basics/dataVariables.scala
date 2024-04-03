cea8e7b55f9da26f99584295ad450a27a05a0407
GavDotZip <gavdotzip@gmail.com>
2024-04-03 19:55:25 +0100

    notes

---
+++ src/basics/dataVariables.scala
@@ -0,0 +1,15 @@
+package basics
+
+
+object dataVariables {
+  def main(args: Array[String]): Unit = {
+    // Keyword=var(mutable). Variable Name=a. Data Type=Int. Data Value=12
+    var a : Int = 12
+    println(a)
+
+    println(a+30)
+
+    var b : Int = 12
+    print(a+b+2)
+  }
+}
