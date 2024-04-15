9e7439c7c1d61dd6abb8968442bb52dc23fc1746
GavDotZip <gavdotzip@gmail.com>
2024-04-15 22:23:28 +0100

    If loops

--- src/basics/Demo.scala
+++ src/basics/Demo.scala
@@ -2,23 +2,24 @@ package basics
 
 object Demo {
   def main(args: Array[String]): Unit = {
-    val x = 20;
-    var res = "";
-    if (x == 20) {
+    val x = 20
+    val y = 30
+    var res = ""
+    if (x == 20 && y == 30) { // || for OR statement
       //println("X IS 20");
-      res = "x == 20";
+      res = "x == 20 and y == 30"
     } else {
       //println("X IS NOT 20");
-      res = "x != 20";
+      res = "x != 20"
     }
 
-    println(res);
+    println(res)
 
     // Can also do an If loop like this
-    val res2 = if(x == 20) "x == 20" else "x != 20";
-    println(res2);
+    val res2 = if(x == 20) "x == 20" else "x != 20"
+    println(res2)
     // Also...
-    println(if (x == 20) "x == 20" else "x != 20");
+    println(if (x == 20) "x == 20" else "x != 20")
 
   }
 
