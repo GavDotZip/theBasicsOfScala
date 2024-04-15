b37c05f7a62e8ff4482529aa52385a7bf85672b3
GavDotZip <gavdotzip@gmail.com>
2024-04-15 22:16:26 +0100

    commit

--- src/basics/Demo.scala
+++ src/basics/Demo.scala
@@ -5,12 +5,14 @@ object Demo {
     val x = 20;
     var res = "";
     if (x == 20) {
-      println("X IS 20");
+      //println("X IS 20");
       res = "x == 20";
     } else {
-      println("X IS NOT 20");
+      //println("X IS NOT 20");
       res = "x != 20";
     }
+
+    println(res);
 
   }
 
