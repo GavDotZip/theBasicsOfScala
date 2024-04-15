d3eefc67aaa5ee5ea02f217323534ab501f28c40
GavDotZip <gavdotzip@gmail.com>
2024-04-15 22:15:28 +0100

    commit

--- src/basics/Demo.scala
+++ src/basics/Demo.scala
@@ -2,6 +2,15 @@ package basics
 
 object Demo {
   def main(args: Array[String]): Unit = {
+    val x = 20;
+    var res = "";
+    if (x == 20) {
+      println("X IS 20");
+      res = "x == 20";
+    } else {
+      println("X IS NOT 20");
+      res = "x != 20";
+    }
 
   }
 
