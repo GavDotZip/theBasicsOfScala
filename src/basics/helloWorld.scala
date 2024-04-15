fc8a6bf378fde7ca4a76df6552a92faafd5e0b38
GavDotZip <gavdotzip@gmail.com>
2024-04-15 22:05:04 +0100

    commit

--- src/basics/helloWorld.scala
+++ src/basics/helloWorld.scala
@@ -7,6 +7,8 @@ object helloWorld {
     val age = 31
     println(name + " is " + age + " years old")
     // Can alsa do the following...
-    println(s"$name is $age years old")
+    println(s"$name is $age years old") // S String Interpolation
+    // Can also do the following for a type safe manner...
+    println(f"$name%s is $age%d years old") // F String Interpolation
   }
 }
