3d27aa8b9ba34d9fb30a4b6bc810c166409422c3
GavDotZip <gavdotzip@gmail.com>
2024-04-17 00:00:30 +0100

    commit

--- src/projects/beginner/A1/Tester.scala
+++ src/projects/beginner/A1/Tester.scala
@@ -2,8 +2,11 @@ package projects.beginner.A1
 import scala.io.StdIn // Works similar to Scanner in Java
 
 case class Tester(name: String, id: Int) {
-  def main(args: Array[String]): Unit = {
-    
+  object Tester extends App {
+    // Read in the number of students to be printed
+    println("Welcome! Personalized Student Class Tester initialized...")
+    println("How many students would you like to add?")
+    val numberOfStudents = StdIn.readInt()
   }
 
 }
