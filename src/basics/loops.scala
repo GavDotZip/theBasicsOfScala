bae63cfbcd826e6f27f975a90b86f796033a08de
GavDotZip <gavdotzip@gmail.com>
2024-04-24 22:54:49 +0100

    commit

--- src/basics/loops.scala
+++ src/basics/loops.scala
@@ -3,12 +3,20 @@ package basics
 object loops {
   def main(args: Array[String]): Unit = {
     var x =0;
-    
+
+    // While Loop
     while(x<10) {
       println("x = " + x)
       x += 1; //add 1 to existing value of x
     }
-    
+
+    // Do-While Loop
+    var y = 0;
+    do {
+      println("y = " + y)
+      y += 1;
+    } while (y<10);
+
   }
 
 }
