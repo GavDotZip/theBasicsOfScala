d46676697e851f71a67d611e4e6285aaace9bc88
GavDotZip <gavdotzip@gmail.com>
2024-04-29 23:10:02 +0100

    commit

--- src/basics/scalaFunctions.scala
+++ src/basics/scalaFunctions.scala
@@ -2,12 +2,7 @@ package basics
 
 object scalaFunctions {
 
-  object Math { // If it's a new class, we need new instance of object. Here, it is just a new object
-    def add(x: Int, y: Int): Int = {
-      return x + y
-    }
-    def square(x : Int) = x * x
-  }
+
   // If a block of code is repeating in program, best declare as a function
   // function takes form: def functionName(argument : dataType of argument) : dataType of Return
   def add(x : Int, y : Int) : Int = {
@@ -22,13 +17,39 @@ object scalaFunctions {
 
   def divide(x: Int, y: Int) = x / y // No need to add return type if certain of return data type
 
+  object Math { // If it's a new class, we need new instance of object. Here, it is just a new object
+    def add(x: Int = 45, y: Int = 15): Int = { // default values for parameters
+      return x + y
+    }
+
+    def +(x: Int, y: Int): Int = { // Can use operators as function names
+      return x + y
+    }
+
+    def **(x: Int) = x * x
+
+    def square(x: Int) = x * x
+  }
+
+  def print(x : Int, y : Int): Unit = {
+    println(x+y);
+  } // Function which does not return anything uses 'Unit'
+
   def main(args: Array[String]): Unit = {
-    println(Math.add(45, 15)) // Call the add function from Math object
+    val sum = 10 + 20 // this + is also a function
+    println(Math ** 3)
+    print(100, 200)
+    println(Math.add(60, 20)) // Call the add function from Math object
     println(Math square 3) // Call the square function from Math object
 
     println(add(45, 15)) // Call the add function
     println(subtract(45, 15)) // Call the subtract function
     println(multiply(45, 15)) // Call the multiply function
     println(divide(45, 15)) // Call the divide function
+
+    // functions are treated as first class citizens: can assign function
+    // to a variable using anonymous functions
+    var addAnon = (x : Int, y : Int) => x + y
+    println(addAnon(300, 500))
   }
 }
