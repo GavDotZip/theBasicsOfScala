a3185674ffaed3108b6c85291a5c749bfa230a77
GavDotZip <gavdotzip@gmail.com>
2024-04-29 22:30:07 +0100

    commit

--- src/basics/matchExpressions.scala
+++ src/basics/matchExpressions.scala
@@ -28,6 +28,12 @@ object matchExpressions {
 
     println("result = " + result)
 
+    // Evaluate Multiple Cases
+    val i = 6
+    i match {
+      case 1 | 3 | 5 | 7 | 9 => println("odd")
+      case 2 | 4 | 6 | 8 | 10 => println("even")
+    }
 
   }
 }
